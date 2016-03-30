765ff1cdde13de81f7ee83f0a88e333ef6bb339a
BeckyLambert <beckylambert@outlook.com>
2016-03-30 12:48:31 -0600

    added methods to answers

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,40 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# “iNvEsTiGaTiOn”.<???>
-# => “InVeStIgAtIoN”
+puts "iNvEsTiGaTiOn".swapcase
+# => "InVeStIgAtIoN"
 
-# “zom”.<???>
-# => “zoom”
+puts "zom".gsub(/o/, 'oo')
+# => "zoom"
 
-# “enhance”.<???>
-#=> “    enhance    ”
+puts "enhance".center(15)
+# => "    enhance    "
 
-# “Stop! You’re under arrest!”.<???>
-# => “STOP! YOU’RE UNDER ARREST!”
+puts "Stop! You’re under arrest!".upcase
+# => "STOP! YOU’RE UNDER ARREST!"
+# This is one of the culprits - ALL UPPER CASE!
 
-# “the usual”.<???>
-#=> “the usual suspects”
+puts "the usual".concat(" suspects")
+#=> "the usual suspects"
 
-#“ suspects”.<???>
-#=> “the usual suspects”
+puts " suspects".prepend("the usual")
+# => "the usual suspects"
 
-# “The case of the disappearing last letter”.<???>
-# => “The case of the disappearing last lette”
+puts "The case of the disappearing last letter".chop
+# => "The case of the disappearing last lette"
+# This is the other culprit - chopping off the last letter!
 
-# “The mystery of the missing first letter”.<???>
-# => “he mystery of the missing first letter”
+puts "The mystery of the missing first letter".reverse.chop.reverse
+# => "he mystery of the missing first letter"
 
-# “Elementary  ,    my   dear      Watson!”.<???>""
-# => “Elementary, my dear Watson!”
+puts "Elementary  ,    my   dear      Watson!".squeeze(" ").gsub(' ,', ',')
+# => "Elementary, my dear Watson!"
 
-#“z”.<???>
+puts "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+# It's the ordinal number of this one character string.
 
-#“How many times does the letter ‘a’ appear in this string?”.<???>
-#=> 4
+
+puts "How many times does the letter ‘a’ appear in this string?".count "a"
+# => 4
