24e7cae66607e84965b722fbf855b709868c04a2
BeckyLambert <beckylambert@outlook.com>
2016-04-02 22:10:44 -0600

    release 2 finished

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,26 @@
+# This is an example of taking an order for food and beer where I work 
+
+prost = {
+    food: {
+        sausages: 'Four Sampler',
+        sampler_info: [
+            "elk", "buffalo", "boar", "pheasant" 
+        ],
+        sides: [
+            "potato salad", "cucumber salad", "chips"
+        ]
+    },
+    beers: {
+        type: 'German',
+        options: [
+            "Hofbrau Dunkel",
+            "Hofbrau Marzen"
+            ]
+    },
+    bar_info: {
+        company_name: 'Ein Prosit',
+        seating_info: {
+            total_tables: 8,
+            seats_available: 70
+        },
+}
