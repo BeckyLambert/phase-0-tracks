078a797c9f5fcc90cd55fd6722f8b7f26b23a582
BeckyLambert <beckylambert@outlook.com>
2016-04-09 17:48:47 -0600

    completed gps solo

--- gps2_2.rb
+++ gps2_2.rb
@@ -25,3 +25,62 @@
 # input:
 # steps:
 # output:
+
+def grocery_list (str)
+  food_list = Hash.new(0)
+  
+  food_array = str.split(" ")
+  food_array.each do |item|
+    food_list[item] = 1
+  end
+  
+  printpretty(food_list)
+
+end
+
+def add_item (list, item, quantity)
+  list[item] = quantity
+  printpretty(list)
+end
+
+def remove_item (list, item)
+  list.delete(item)
+  printpretty(list)
+end
+
+def update (list, item, quantity)
+  list[item] = quantity
+  printpretty(list)
+end
+
+def printpretty (list)
+  list.each do |item, quantity|
+    puts "You are buying #{quantity} of #{item}"
+  end
+end
+
+
+example_string = "carrots apples cereal pizza"
+example_list = grocery_list(example_string)
+example_list
+add_item(example_list, "bananas", 7)
+example_list
+remove_item(example_list, "apples")
+update(example_list, "pizza", 5)
+
+
+
+##### HASH SYNTAX
+
+## STRING
+# hash = {
+#   "string_key" => value
+#   }
+
+# ## SYMBOL
+# hash = {
+#   symbolkey: value
+#   }
+
+# ### UPDATES OR CREATES KEY, VALUE PAIR (FOR STRING OR SYMBOL SYNTAX)
+# hash[key] = value
