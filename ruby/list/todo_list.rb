40f5c6af9afa9517409ec39afd39dc26adff02a5
BeckyLambert <beckylambert@outlook.com>
2016-04-10 12:54:17 -0600

    finished 6.6

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,15 @@
+class list 
+   def get_items
+   	array = Array.new
+   	array = ["do the dishes", "mow the lawn"]
+   end
+
+   def add_items
+   end
+
+   def delete_items
+   end
+
+   def get_item
+   end 
+end 
