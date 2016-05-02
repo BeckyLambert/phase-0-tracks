55431edeada42cc2edc8ac1fce6f394a88e5765f
BeckyLambert <beckylambert@outlook.com>
2016-05-02 13:34:47 -0600

    greet child method created and driver code added

--- becky_m_lambert/parent.rb
+++ becky_m_lambert/parent.rb
@@ -1,8 +1,15 @@
 class Parent
 
+attr_accessor :name
+attr_reader :hours
+
 	def initialize(name, hours)
 		@name = name
 		@hours = hours
+	end
+
+	def greet_child
+		p "*yaaaawn* I'm up, I'm up. #{name} is up. Whats the matter"
 	end
 end 
 
