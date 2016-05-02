d597bb588aa3ed55d8e1e6ef0ba11afee9ce0c51
BeckyLambert <beckylambert@outlook.com>
2016-05-02 14:45:57 -0600

    question 8 completed

--- becky_m_lambert/parent.rb
+++ becky_m_lambert/parent.rb
@@ -40,3 +40,27 @@ parent.hear_request("can you make breakfast")
 parent.hear_request("can we get a puppy")
 parent.hear_request("I want a unicorn!")
 parent.display_request_history
+
+#Question 8:
+# This was as close as I could get. I cant get it to print anything but an empty count though.
+# def count_vowels(string)
+#  requests = ["please buy me a toy", "I need to pass this assesment", "can you make breakfast", "can we get a puppy", "I want a unicorn!"]
+#   i = 0
+#   j = 0
+#   count = 0
+
+#   while i < string.length do
+#     while j < requests.length do
+#       if string[i] == requests[j]
+#         count += 1
+#         break
+#       end
+
+#       j += 1
+#     end
+
+#     i += 1
+#   end
+
+# count_vowels
+# end 
