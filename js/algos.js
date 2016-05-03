86498f384eaa82a29d65b67828d19ea24c50f13a
BeckyLambert <beckylambert@outlook.com>
2016-05-03 14:10:30 -0600

    added more psuedocode and test code for release 2

--- js/algos.js
+++ js/algos.js
@@ -1,24 +1,22 @@
 // Release 0
 
-function longest_phrase(arrayOfStrings) {
+function longestPhrase(arrayOfStrings) {
 	// Pseudocode:
  	// This function will return the longest phrase
  	// If two phrases are identical length,
  	// it'll return only the first.
  	// Declare string var called longestSoFar, initialize to ""
- 	var longest_word = "";
+ 	var longestWord = "";
 
   	for (var i = 0; i < arrayOfStrings.length; i++) {
-  		// check for anything longer than longest_yet,
- 		if (arrayOfStrings[i].length > longest_word.length) {
-		  	// if so then push it into longest_yet
-		  	 longest_word = arrayOfStrings[i];
- 		} else {
- 			// do nothing
+  		// check for anything longer than longestYet,
+ 		if (arrayOfStrings[i].length > longestWord.length) {
+		  	// if so then push it into longestYet
+		  	 longestWord = arrayOfStrings[i];
  		}
   	}
- 	// At the end, return longest_yet
- 	return longest_word;
+ 	// At the end, return longestYet
+ 	return longestWord;
 }
 
 
@@ -40,42 +38,55 @@ function findMatch(person1, person2) {
 			console.log("True, " + key + " is " + person1[key] + " for both.");
 			 returnValue = true;
 		}
-	
- 
-	// At end, return returnValue
-	return returnValue
+		// At end, return returnValue
+	return returnValue;
     }
 }
 
 // Release 2
-function generate_randomness(how_many) {
-	// Takes the argument how_many, loops it that many times, each loop generates
+function generateRandomness(howMany) {
+	// Takes the argument howMany, loops it that many times, each loop generates
 	// a word filled with random letters.
 	// Declare empty array randomWords
 	var randomness = [];
-	// Declare var theAlphabet of "abcdefghijklmnopqrstuvwxyz"
+	// Declare var Alphabet of "abcdefghijklmnopqrstuvwxyz"
 	var alphabet = "abcdefghijklmnopqrstuvwxyz";
-	for (var w = 0; w < how_many; w++) {
+	for (var w = 0; w < howMany; w++) {
 
-	  //   Declare an empty variable equal to this_word
-	  var this_word = "";
+	  //   Declare an empty variable equal to thisWord
+	  var thisWord = "";
 	  //   Declare a variable and set it equal to wordLength, 
 	  //   which is a random number from 1 thru 10
 	  var wordLength = Math.floor((Math.random() * 10) + 1);
 	   console.log("Word length is " + wordLength);
 
 	  for (var c = 0; c < wordLength; c++) {
-	    var theIndex = (Math.floor((Math.random() * 26) + 1) - 1)
-	    this_word = this_word + alphabet.charAt(theIndex);
+	    var theIndex = (Math.floor((Math.random() * 26) + 1) - 1);
+	    thisWord = thisWord + alphabet.charAt(theIndex);
 	    }
 
 	  //   Add thisWord to randomWords
-	  randomness.push(this_word);
+	  randomness.push(thisWord);
 	}
 
-	// When done with all that, return randomWords
+	// When done with that, return randomWords
 	return randomness;
 }
+
+
+
+
+// Release 0 driver code:
+var testArray = ["long phrase","longest phrases","longer phrase"];
+console.log("Test array: " + testArray);
+console.log("The longest phrase is: " + longestPhrase(testArray));
+
+// Release one driver code:
+person1 = {name: "Becky", age: 29, faveColor: "blue"};
+person2 = {name: "Elisa", age: 30, faveColor: "green"};
+
+// Release 2 test code:
+
 // Set up for loop to run through 10 times.
 for (var i = 0; i < 10; i++) {
 
@@ -83,21 +94,11 @@ for (var i = 0; i < 10; i++) {
 	console.log("");
 
 	// Generate & print the 5 random words
-	var my_words = generate_randomness(5);
-	console.log("For these 5 words: " + my_words);
+	var myWords = generateRandomness(5);
+	console.log("For these 5 words: " + myWords);
 
 	// Find & print the longest one of those 5
-	var myLongest = longest_phrase(my_words);
+	var myLongest = longestPhrase(myWords);
 	console.log("the longest one is: " + myLongest);
 }
 console.log("");
-
-
-// Release 0 driver code:
-var testArray = ["long phrase","longest phrases","longer phrase"];
-console.log("Test array: " + testArray)
-console.log("The longest phrase is: " + longest_phrase(testArray));
-
-// Release one driver code
-person1 = {name: "Becky", age: 29, faveColor: "blue"};
-person2 = {name: "Elisa", age: 30, faveColor: "green"};
