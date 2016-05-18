4973c0dc59c8a5c3886bec49d660ec6336799f6d
BeckyLambert <beckylambert@outlook.com>
2016-05-18 13:00:13 -0600

    fixed punctuation and spelling errors in the psuedocode

--- Booster_Videos/robot_translator.rb
+++ Booster_Videos/robot_translator.rb
@@ -1,11 +1,11 @@
 # ROBOT TRANSLATOR
 
-#If a letter is capitalized and its in the first hald of the 
+#If a letter is capitalized and it's in the first half of the 
 #alphabet, it becomes "bloop".
 
-#Otherwise if a letter is capitalized or its the letter "e", it becomes "buzz".
+#Otherwise if a letter is capitalized or it's the letter "e", it becomes "buzz".
 
-#If its not a letter at all, it becomes "boing".
+#If it's not a letter at all, it becomes "boing".
 
 #Otherwise it becomes "beep".
 
@@ -15,18 +15,18 @@
 
 def translate_char(char)
   alphabet = "abcdefghijklmnopqrstuvwxyz"
-  halfway = alphabet.length / 2 	
+  halfway = alphabet.length / 2   
   is_capitalized = (char.upcase == char)
   index_of_char = alphabet.index(char.downcase)
 
    if !index_of_char 
-   	"boing"
+    "boing"
    elsif is_capitalized && index_of_char < halfway
-   	 "bloop"
-   	elsif is_capitalized || char == "e"
-   	 "buzz"
-   	else
-   	 "beep"
+     "bloop"
+    elsif is_capitalized || char == "e"
+     "buzz"
+    else
+     "beep"
    end 
 end 
 
@@ -35,7 +35,7 @@ def translate_phrase(phrase)
    translated_response = ""
    while char_index < phrase.length
    translated_response << translate_char(phrase[char_index])
-   	char_index += 1
+    char_index += 1
    end 
    translated_response
 end 
@@ -48,6 +48,9 @@ inputted_phrase = gets.chomp
 break if inputted_phrase == 'q'
 puts translate_phrase (inputted_phrase)
 end 
+
+
+
 
 
 
